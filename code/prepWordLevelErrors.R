789ef09065a34dbbe37fda975dbb513b5a563795
l-acs <lucas.sahar@mail.mcgill.ca>
2024-03-23 13:38:29 -0400

    Compute error rates by participant, longer and with percentages

--- code/prepWordLevelErrors.R
+++ code/prepWordLevelErrors.R
@@ -125,7 +125,7 @@ percentize_multiple <- function(df, cols) {
 append_sd_as_last_row <- function(df, cols_to_sd, id_col = NULL) { # todo trim
   cols <- df %>% select({{cols_to_sd}}) %>% colnames
 
-  if(is.na(id_col) || is.null(id_col)) {
+  if(missing(id_col) || is.na(id_col) || is.null({{id_col}})) {
     id_col <- # set it to the first col not in passed list
       df %>%
       colnames %>%
@@ -137,11 +137,11 @@ append_sd_as_last_row <- function(df, cols_to_sd, id_col = NULL) { # todo trim
 
   # cols is a tidyselection
   compute_sd_if_in_cols_else_na <- function(col) {
-    if (col %in% cols) {
-      df %>% pull(col) %>% sd(na.rm = TRUE)
-    } else if (col == id_col) { # cell reflects that this row is the sd row
+    if (col == id_col) { # cell reflects that this row is the sd row
       "sd" # a label- other cells in this column might have "misprd", "hes", etc
-    } else {
+    } else if (col %in% cols) { # or it's what we want to actually compute sd on
+      df %>% pull(col) %>% sd(na.rm = TRUE)
+    } else { # otherwise truly not applicable
       NA
     }
   }
@@ -151,7 +151,11 @@ append_sd_as_last_row <- function(df, cols_to_sd, id_col = NULL) { # todo trim
     data.frame %>%
     setNames(colnames(df))
 
-  add_row(df, sd_row)
+  # since we want to put the literal "sd" in it, we need to make sure it's a
+  # character column
+  df %>%
+    mutate(across({{id_col}}, as.character)) %>%
+    add_row(sd_row)
 }
 
 
@@ -169,7 +173,6 @@ rates_long_with_percents <-
   rates_long %>%
   mutate(rate_as_percent = percentize(rate_of_error_type)) %>%
   append_sd_as_last_row(rate_of_error_type)
-
 
 long_data_by_passage <- # compute mean, i.e. rate of occurrence, by error type
   preprocessed_data_with_pan_error_col %>%
@@ -199,57 +202,20 @@ long_data_by_passage %>%
   rowMeans()
 
 
-# todo rewrite w/o sd as column, per above
-long_data_by_participant <- # does sd mean anything meaningful here??
-  preprocessed_data %>%
+long_data_by_participant <- # rates of each error type for each person
+  preprocessed_data_with_pan_error_col %>%
   reframe(
-    across(misproduction:correction, mean_and_sd, .unpack = TRUE),
-    .by = participant_id
-  ) %>% pivot_mean_and_sd_longer()
-
-
-# rates of each error type for each person
-rates_by_participant <- preprocessed_data %>%
-  group_by(participant_id) %>%
-  summarize(across(misproduction:correction,
-                   \(x) length(which(x)) / n(),
-                   .names = "{.col}_rate")) %>%
-  select(participant_id, ends_with("_rate"))
-
-
-# better:
-preprocessed_data %>%
-  reframe(
-    across(misproduction:correction, mean_and_sd, .unpack = TRUE),
-    .by = participant_id
-  ) %>%
-  pivot_mean_and_sd_longer(.to_percent = TRUE) %>%
-  select(-sd)
-view_last_if()
-
-# "", sd
-rates_by_participant %>%
-  summarize(across(ends_with("_rate"),
-                   \(x) sd(x, na.rm = TRUE),
-                   .names = "{.col}_sd"))
-
-# rates of each error type for each passage
-rates_by_passage <- preprocessed_data %>%
-  group_by(passage) %>%
-  summarize(across(misproduction:correction,
-                   \(x) length(which(x)) / n(),
-                   .names = "{.col}_rate")) %>%
-  select(passage, ends_with("_rate"))
-
-# "", sd
-rates_by_passage %>%
-  summarize(across(ends_with("_rate"),
-                   \(x) sd(x, na.rm = TRUE),
-                   .names = "{.col}_sd"))
-
+    across(misproduction:correction|any_error:any_error_except_omission,
+           \(.) mean(., na.rm = TRUE)),
+    .by = participant_id) %>%
+  percentize_multiple(c(where(is.numeric), -participant_id)) %>% # include as %s
+  append_sd_as_last_row(c(where(is.numeric), -participant_id)) %>% # get our sd
+  select(-where(is.numeric), where(is.numeric)) %>% # %s first, for readability
+  transpose(keep.names = "error_type", make.names = "participant_id") %>%
+  as_tibble() # for printing/dev/interactive (this is what it was pre transpose)
 
 # join participant error data and counterbalance data
-preprocessed_data_by_condition <- preprocessed_data %>%
+preprocessed_data_by_condition <- preprocessed_data_with_pan_error_col %>%
   left_join(counterbalance_data, by = "participant_id") %>%
   mutate(
     which_passage_set = str_sub(category, -1, -1), # "a" or "b"
@@ -257,11 +223,11 @@ preprocessed_data_by_condition <- preprocessed_data %>%
       (which_passage_set == "a" &  set_a_alone) |
       (which_passage_set == "b" & !set_a_alone),
     social = !alone
-  ) %>% select(colnames(preprocessed_data), social)
+  ) %>% select(colnames(preprocessed_data_with_pan_error_col), social)
 
 
 
-# rates of each error type by condition
+# rates of each error type by condition- fixme per above
 rates_by_condition <- preprocessed_data_by_condition %>%
   group_by(social) %>%
   summarize(across(misproduction:correction,
